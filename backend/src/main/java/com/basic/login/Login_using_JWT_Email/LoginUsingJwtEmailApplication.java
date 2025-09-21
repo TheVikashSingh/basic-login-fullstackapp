f871bf5b5ba20b45f3082bcdf20a71e2b1d9f5df
= <=>
2025-09-21 08:31:37 +0530

    Committing basic skeleton of project's backend

---
+++ backend/src/main/java/com/basic/login/Login_using_JWT_Email/LoginUsingJwtEmailApplication.java
@@ -0,0 +1,13 @@
+package com.basic.login.Login_using_JWT_Email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginUsingJwtEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginUsingJwtEmailApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/basic/login/Login_using_JWT_Email/LoginUsingJwtEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.login.Login_using_JWT_Email;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginUsingJwtEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
